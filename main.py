52e900081f6ef1072639b28369c0962e2ebd447a
Frank Rumreich <105813222+rumreichf@users.noreply.github.com>
2022-09-02 15:36:56 +0000

    printed name for lab

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from pygame.locals import QUIT
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
+
+print("Hello Frankie")
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+
