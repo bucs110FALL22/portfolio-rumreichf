6224026e77ab1f8cb8e0f47d606d5829c8f33eb6
Frank Rumreich <105813222+rumreichf@users.noreply.github.com>
2022-09-07 22:07:49 +0000

    started activity

---
+++ ch02/exercises/myturtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+myturtle = turtle.Turtle()
+num_sides = 4
+sidelength = 50
+angle = 360 / num_sides
+
+myturtle.color("purple")
+myturtle.shape("turtle")
+
+for 2
+  for num_sides
+    myturtle.forward(50)
+    myturtle.right(angle)
