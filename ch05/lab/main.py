87a7e03c6d72b83305beb49465d14ad258f8b77a
Frank Rumreich <105813222+rumreichf@users.noreply.github.com>
2022-12-09 23:27:41 +0000

    lab ch05

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,10 +1,12 @@
 import pygame
-
+pygame.init()
 scale = 5
+max_so_far = 0
+
 #pygame.font.init = True
 display = pygame.display.set_mode()
 count = 0
-#font = pygame.font.Font(None, 14)
+font = pygame.font.Font(None, 14)
 upper_limit = 20
 iters = {}
 max_so_far = 0
@@ -25,11 +27,18 @@ def n_sequence(n):
 #----------------------------------------------------------------
   
 for start in range(2, upper_limit+1):
-#  count = 0
+  # count = 0
+  display.fill("black")
   iters[start] = n_sequence(start)
+  if n_sequence(start) > max_so_far:
+    max_so_far = n_sequence(start)
+print(iters)
+print(iters.items())
 if len(iters) > 1:
-  pygame.draw.lines(display, 'white', False, (list(iters.items())*scale))4
-  
+  pygame.draw.lines(display, "white", False, (tuple(iters.items())))
+  print(tuple(iters.items()))
 new_display = pygame.transform.flip(display, False, True)
+msg = font.render(str(max_so_far), False, "white")
+display.blit(msg, (10,10))
 display.blit(new_display, (0, 0))
 
